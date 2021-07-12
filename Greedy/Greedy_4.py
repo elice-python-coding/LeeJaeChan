1423b464ba703bd6e0b5f5c855ec7fadb573cd87
Student_Lee <ljaechan1928@gmail.com>
2021-07-12 13:08:28 +0900

    add Greedy_4.py

---
+++ Greedy/Greedy_4.py
@@ -0,0 +1,13 @@
+N, K = map(int, input().split())
+n_count = 0
+
+while N != 1:
+    if N % K != 1 and (N % K) == 0:
+        N = N // K
+        n_count += 1
+    elif (N % K) >= 1:
+        N -= 1
+        n_count += 1
+
+
+print(n_count)
