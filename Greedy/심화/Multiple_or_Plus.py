dc22a05a57890ab943130b6e1dbc87693ae3f251
Student_Lee <ljaechan1928@gmail.com>
2021-07-14 20:48:44 +0900

    Modification Multiple_or_Plus.py 14.7.21

--- Greedy/심화/Multiple_or_Plus.py
+++ Greedy/심화/Multiple_or_Plus.py
@@ -1,10 +1,11 @@
 s = input()
-result = int(s[0])
+result = int(s[0])              # s의 0번째 인덱스(계속해서 값을 더해갈 기준 인덱스)
 
 for i in range(1, len(s)):
-    num = int(s[i])
-    if result <= 1 or num <= 1:
+    num = int(s[i])             # 1씩 증가, 인덱스 끝까지 확인
+    if result <= 1 or num <= 1: # 0, 1일 때는 *보다 +가 더 높은 값
         result += num
     else:
-        result *= num
+        result *= num           # 0, 1이 아닐 때는 전부 곱셈 연산
+
 print(result)
