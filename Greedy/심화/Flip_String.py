b7c224aedca63f1e6102a2622df09bf515d901d6
Student_Lee <ljaechan1928@gmail.com>
2021-07-14 22:32:23 +0900

    Modification Flip_String.py 14.7.21

--- Greedy/심화/Flip_String.py
+++ Greedy/심화/Flip_String.py
@@ -3,7 +3,9 @@ ZeroCount = 0
 OneCount = 0
 
 for i in range(len(s)-1):
+    # 현재 인덱스와 다음 인덱스가 같지 않다면 실행, 같다면 pass
     if s[i] != s[i+1]:
+        # 이 때, 0이라면 1이 적고, 1이라면 0이 적다
         if s[i+1] == '0':
             OneCount += 1
         else:
